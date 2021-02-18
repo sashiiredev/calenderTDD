6ad4fed22ed7bfde39261c9a6aadb39a3ae46647
habibaHamdane <haboubalouaiy@gmail.com>
2021-02-18 13:55:31 +0100

    add class category.ts, event.spec.ts

--- calender/calenderTDD/src/entities/Event.ts
+++ calender/calenderTDD/src/entities/Event.ts
@@ -1,20 +1,10 @@
 
-export interface IEvent{
-    id: number;
-    title: string;
-    heureD: Date;
-    heureF: Date;
-    locality: string;
-    description: string;
-}
-
-export class Event implements IEvent{
-    constructor(public id: number,
+export class Event {
+    constructor(
         public title: string,
         public heureD: Date,
         public heureF: Date,
         public locality: string,
-        public description: string){
-
-    }
+        public description: string,
+        public id?: number){}
 }
